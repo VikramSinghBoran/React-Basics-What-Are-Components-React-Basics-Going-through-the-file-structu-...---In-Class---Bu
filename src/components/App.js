21541927184fa09a1bb05e134c4640fa7723bafa
Vikram Singh Boran <vikramsinghboran98@gmail.com>
2022-02-22 22:42:55 +0530

    Added event to button for para render

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,53 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
+  // const [flag,setFlag] = useState(true);
+  // const  showP = (prevFlag)=>{
+  //   if(prevFlag == true){
+  //     setFlag(false);
+  //     console.log(flag);
+  //   }
+  //   console.log(typeof prevFlag);
+  // }
+  const [content,setContent] = useState();
+  
+  const handleClick = ()=>{
+    setContent(<p id="para">"Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy"</p>);
+  }
   return (
     <div id="main">
-      // Do not alter the main div
+      {/* // Do not alter the main div */}
+      <button id="click" onClick={handleClick}>Click</button>
+      {/* <p>{content}</p> */}
+      {content}
     </div>
   );
 }
+
+/*
+
+const [flag,setFlag] = useState(true);
+
+  showP = (prevFlag)=>{
+    if(prevFlag === true){
+      setFlag(false);
+    }
+    // else{
+    //   setFlag(true);
+    // }
+  }
+  return (
+    <div id="main">
+      // Do not alter the main div
+      <button id="click" onClick={(prevFlag)=>{
+        return showP(prevFlag);
+      }}>Click</button>
+      {prevFlag?<p>Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>:<></>}
+
+    </div>
+  );
+
+*/
 
 
 export default App;
